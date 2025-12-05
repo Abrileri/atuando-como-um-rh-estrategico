2994da24b8713dfcc61ab182d13d460635dd4db1
Marcelo Abrileri <34251722+Abrileri@users.noreply.github.com>
2025-12-05 10:51:40 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
-export default defineConfig({
-  base: '/atuando-como-um-rh-estrategico/'
-});
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
+export default defineConfig({
+  base: "./", // <- ISSO É O SEGREDO PRA RODAR NO CLIQUE
+  plugins: [react()],
+});
